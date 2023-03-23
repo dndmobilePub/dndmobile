35cac5e780ec5c8f179c71c7d9a1e3f2d0cf28bf
misunyang <misunyang@dndmobile.co.kr>
2023-03-23 15:03:35 +0900

    공통 추가

--- dist/assets/js/common.js
+++ dist/assets/js/common.js
@@ -1,71 +1,68 @@
-console.log(gsap)
 
-// gsap.to('.box',1 ,{
-//   x:'300px'
-// })
-/**
- * ==============================+
- * 토글버튼 정의
- * ==============================+
- */
-
-const t1 = gsap.timeline({paused: true});
 const menuToggle = document.getElementById("menuToggle");
-const menuBar  = gsap.timeline();
-menuBar.to('.bar-1', 0.5, {
-  attr: {d:"M8,2 L2,8"},
-  x:1,
-  ease:Power2.easeInOut
-},'start')
+const menuBar = gsap.timeline();
+var tl = gsap.timeline({ paused: true});
 
-menuBar.to('.bar-2',0.5, {
-  autoAlpha:0
-},'start')
+menuBar.to('.bar-1', 0.5,{
+	attr:{d: "M8,2 L2,8"},
+	x:1,
+	ease: Power2.easeInOut
+}, 'start')
 
-menuBar.to('.bar-3', 0.5, {
-  attr: {d:"M8,8 L2,2"},
-  x:1,
-  ease:Power2.easeInOut
-},'start')
+menuBar.to('.bar-2', 0.5,{
+	autoAlpha: 0
+}, 'start')
 
-menuToggle.addEventListener('click',function() {
-  menuBar.reversed(!menuBar.reversed());
-  t1.reverse(!t1.reversed())
-});
+menuBar.to('.bar-3', 0.5,{
+	attr:{d: "M8,8 L2,2"},
+	x:1,
+	ease: Power2.easeInOut
+}, 'start')
 
 menuBar.reverse();
-t1.reverse();
-
-/**
- * ==============================+
- * 토글버튼 정의
- * ==============================+
- */
-t1.fromTo('.fullpage-menu', 1, {
-  duration:1,
-  display:"block",
-  x:'0%',
-  y:'0%',
-  stagger:0.1,
-  ease:'Expo.easeInOut'
-},
-{
-  x:'100%',
-  y:'0%',
-})
-
-// t1.from('.fullpage-menu', {
-//   duration:1,
-//   display:"block",
-//   x:'100%',
-//   stagger:0.1,
-//   ease:'Expo.ease'
-// })
 
 
-// t1.from('.fullpage-menu', {
-//   duration:1.5,
-//   y:'100%',
-//   stagger:0.2,
-//   ease:'Expo.ease'
-// })
+tl.to('.fullpage-menu', {
+	duration:0,
+	display: "block",
+	ease: 'Expo.easeInOut',
+});
+
+tl.from('.menu-bg span', {
+	duration:0.4,
+	x:"100%",
+	stagger: 0.1,
+	ease: 'Expo.easeInOut'
+});
+
+tl.from('.main-menu li a', {
+	duration:1,
+	y:"100%",
+	stagger: 0.2,
+	ease: 'Expo.easeInOut'
+} , "-=0.5");
+
+tl.from('.line', {
+	duration:0.3,
+	scaleX: 0,
+	transformOrigin: "0px 0px",		
+});
+
+tl.from('.social-links li', {
+	duration: 1,
+	y:"-100%",
+	opacity:0,
+	stagger: 0.1,
+	ease: 'Expo.easeInOut'
+} , "-=0.5");
+
+
+tl.reverse();
+
+menuToggle.addEventListener('click', function(){
+	menuBar.reversed(!menuBar.reversed());
+	tl.reversed(!tl.reversed());
+  menuToggle.classList.toggle('active');
+
+});
+
