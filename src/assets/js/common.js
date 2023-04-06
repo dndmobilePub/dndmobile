676e273ac3ca8ffd8d2f184cb7a67a340a7dca7b
dnd-dahye <dahyelee@dndmobile.co.kr>
2023-04-06 12:06:13 +0900

    소스 정리 및 수정

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -125,3 +125,22 @@ window.addEventListener('scroll', () => {
 	}
 })
 
+
+/**
+ * ==============================+
+ * 푸터 정의
+ * ==============================+
+ */
+let mybutton = document.getElementById("topBtn");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+function topFunction() {
+	window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
